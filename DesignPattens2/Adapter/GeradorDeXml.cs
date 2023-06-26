1fe55a2619f8cda45c2d6aa7b26f5e15500416a9
Bonox23 <ricardo.bonos@gmail.com>
2023-06-26 17:19:25 -0300

    Adapter

---
+++ DesignPattens2/Adapter/GeradorDeXml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace DesignPattens2.Adapter
+{
+    public class GeradorDeXml
+    {
+        public string GeraXml(Object objeto)
+        {
+            XmlSerializer serializer = new XmlSerializer(objeto.GetType());
+            StringWriter writer = new StringWriter();
+            serializer.Serialize(writer, objeto);
+            return writer.ToString();
+        }
+    }
+}
---
+++ DesignPattens2/Adapter/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattens2.Adapter
+{
+    public class Program
+    {
+        static void Main(String[] args)
+        {
+            Cliente cliente = new Cliente();
+            cliente.Nome = "victor";
+            cliente.Endereco = "Rua Vergueiro";
+            cliente.DataDeNascimento = DateTime.Now;
+
+            String xml = new GeradorDeXml().GeraXml(cliente);
+            Console.WriteLine(xml);
+        }
+    }
+}
